75492924fccde3b09fefd6299f2521a59377f4b8
MohammadAli82 <140317609+MohammadAli82@users.noreply.github.com>
2024-02-18 14:12:36 +0330

    Initial Commit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-'use strict';
-const txt1 = document.getElementById('txt-1')
-const txt2 = document.getElementById('txt-2')
-const txt3 = document.getElementById('txt-3')
-const txt4 = document.getElementById('txt-4')
+"use strict";
+const txt1 = document.querySelector("button");
+
+txt1.addEventListener("click", function () {
+  txt1.classList.remove("p");
+});
