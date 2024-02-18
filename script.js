12bfbf8caffd1be8bd24eeb5ef95886b3a752ad4
MohammadAli82 <140317609+MohammadAli82@users.noreply.github.com>
2024-02-18 13:29:58 +0330

    Initial Commit

---
+++ script.js
@@ -0,0 +1,5 @@
+'use strict';
+const txt1 = document.getElementById('txt-1')
+const txt2 = document.getElementById('txt-2')
+const txt3 = document.getElementById('txt-3')
+const txt4 = document.getElementById('txt-4')
